acbb8bb4ae4cee06c1f9266ad279efe5f1d2f4b8
Annabelle Strong <anstrong@users.noreply.github.com>
2017-12-10 16:08:38 -0500

    Added user feedback

--- toFile.py
+++ toFile.py
@@ -2,6 +2,8 @@
 def sendToFile(string):
 	file = open("Output.txt")
 	file.write(string)
+	print(message + ' - written to file')
+	
 def readFile(lineNumber):
 	file = open("Output.txt","r")
 	return file.read(lineNumber)
