312de26781a563ee466e68b61cc73c49c76f3af3
Brian <sushimaster1999@gmail.com>
2017-12-10 17:00:09 -0500

    please work

--- toFile.py
+++ toFile.py
@@ -1,14 +1,8 @@
 #toFile
 def sendToFile(string):
-<<<<<<< HEAD
+
 	file = open("Output.txt","w")
 	file.write(string + ' - written to file')
-=======
-	file = open("Output.txt")
-	file.write(string)
-	print(message + ' - written to file')
-	
->>>>>>> d62c126439f7240b8a56c654bf694e377890177e
 def readFile(lineNumber):
 	file = open("Output.txt","r")
 	return file.read(lineNumber)
